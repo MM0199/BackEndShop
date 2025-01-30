00e9afbf7ce96d0c9020673339637b8dd9b4a660
MM0199 <pn420360@gmail.com>
2025-01-30 15:02:39 -0600

    Add Cart and CartItem Controller

---
+++ src/main/java/com/tutorial/dreamshops/controller/CartController.java
@@ -0,0 +1,51 @@
+package com.tutorial.dreamshops.controller;
+
+import com.tutorial.dreamshops.exception.ResourceNotFoundException;
+import com.tutorial.dreamshops.model.Cart;
+import com.tutorial.dreamshops.response.ApiResponse;
+import com.tutorial.dreamshops.service.cart.ICartService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("${api.prefix}/carts")
+public class CartController {
+
+    private final ICartService cartService;
+
+    @GetMapping("/get-cart")
+    public ResponseEntity<ApiResponse> getCart(@RequestParam Long id) {
+        try {
+            Cart cart = cartService.getCart(id);
+            return ResponseEntity.ok(new ApiResponse("Found!", cart));
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+        }
+    }
+
+    @DeleteMapping("/clear-cart")
+    public ResponseEntity<ApiResponse> clearCart(@RequestParam Long id) {
+        try {
+            cartService.clearCart(id);
+            return ResponseEntity.ok(new ApiResponse("Cart cleared!", null));
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+        }
+    }
+
+    @GetMapping("/total-price")
+    public ResponseEntity<ApiResponse> getTotalPrice(@RequestParam Long id) {
+        try {
+            BigDecimal totalPrice = cartService.getTotalPrice(id);
+            return ResponseEntity.ok(new ApiResponse("Found!", totalPrice));
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+        }
+    }
+}
---
+++ src/main/java/com/tutorial/dreamshops/controller/CartItemController.java
@@ -0,0 +1,60 @@
+package com.tutorial.dreamshops.controller;
+
+import com.tutorial.dreamshops.exception.ProductNotFoundException;
+import com.tutorial.dreamshops.exception.ResourceNotFoundException;
+import com.tutorial.dreamshops.model.CartItem;
+import com.tutorial.dreamshops.response.ApiResponse;
+import com.tutorial.dreamshops.service.cart.ICartItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("${api.prefix}/cart-items")
+public class CartItemController {
+
+    private final ICartItemService cartItemService;
+
+    @PostMapping("/add")
+    public ResponseEntity<ApiResponse> addItemToCart(@RequestParam Long cartId, @RequestParam Long productId, @RequestParam int quantity) {
+        try {
+            cartItemService.addItemToCart(cartId, productId, quantity);
+            return ResponseEntity.ok(new ApiResponse("Add Item to CartSuccess!", null));
+        } catch (ResourceNotFoundException | ProductNotFoundException e) {
+            return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+        }
+    }
+
+    @DeleteMapping("/remove")
+    public ResponseEntity<ApiResponse> removeItemFromCart(@RequestParam Long cartId, @RequestParam Long productId) {
+        try {
+            cartItemService.removeItemFromCart(cartId, productId);
+            return ResponseEntity.ok(new ApiResponse("Remove Item from CartSuccess!", null));
+        } catch (ResourceNotFoundException | ProductNotFoundException e) {
+            return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+        }
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<ApiResponse> updateItemQuantity(@RequestParam Long cartId, @RequestParam Long productId, @RequestParam int quantity) {
+        try {
+            cartItemService.updateItemQuantity(cartId, productId, quantity);
+            return ResponseEntity.ok(new ApiResponse("Update Item in Cart Success!", null));
+        } catch (ResourceNotFoundException | ProductNotFoundException e) {
+            return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+        }
+    }
+
+    @GetMapping("/get-cart-item")
+    public ResponseEntity<ApiResponse> getCartItem(@RequestParam Long cartId, @RequestParam Long productId) {
+        try {
+            CartItem cartItem = cartItemService.getCartItem(cartId, productId);
+            return ResponseEntity.ok(new ApiResponse("Get Cart Success!", cartItem));
+        } catch (ResourceNotFoundException | ProductNotFoundException e) {
+            return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+        }
+    }
+}
